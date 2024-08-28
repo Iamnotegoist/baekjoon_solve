11f9b13d8128bb50980ffd11d077e04aa3e6135e
kiju9512 <kiju9512@gmail.com>
2024-08-28 16:59:03 +0900

    240828 백준 스터디...

---
+++ Baekjoon_13241.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_13241 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		long N = Integer.parseInt(st.nextToken());
+		long M = Integer.parseInt(st.nextToken());
+		
+		System.out.println(N * M / gcd(N, M));
+		
+	}
+	
+	static long gcd(long n, long m) {
+		if(m == 0) {
+			return n;
+		}
+		
+		return gcd(m, n % m);
+	}
+}
