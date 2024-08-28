cc608ff9c3ebea2775e7342f7c871c732743a635
kiju9512 <kiju9512@gmail.com>
2024-08-28 16:07:31 +0900

    240828 백준 스터디...

--- Baekjoon_1934.java
+++ Baekjoon_1934.java
@@ -1,49 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 import java.util.StringTokenizer;
 
+
 public class Baekjoon_1934 {
-	static Set<Integer> set = new HashSet<Integer>();
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());  
 		
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		
-		set.add(1);
 		
 		for(int i = 0; i < T; i++) {
 			int result = 1;
 			st = new StringTokenizer(br.readLine(), " ");
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
-			
-			for(int j = 1; j <= A; j++) {
-				if(A % j == 0) {
-					set.add(j);
-				}
-			}
-			
-			for(int j = 1; j <= B; j++) {
-				if(B % j == 0) { 
-					set.add(j);
-				}
-			}
-			
-			Iterator<Integer> iterator = set.iterator();
-			
-			while(iterator.hasNext()) {
-				result *= iterator.next();
-			}
-			
-			sb.append(result + "\n");
+			sb.append(A * B / gcd(A, B) + "\n");
 		}
+		
 		System.out.println(sb);
 	}
+	
+	// 재귀 방식으로 최대공약수를 구하는 함수
+	static int gcd(int a, int b) {
+		if(b == 0) {
+			return a;
+		}
+		
+		return gcd(b, a % b);
+	}
+	
+	
 }
---
+++ Baekjoon_2609.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_2609 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken()); 
+		int M = Integer.parseInt(st.nextToken());
+		
+		sb.append(gcd(N, M) + "\n");
+		
+		sb.append(N * M / gcd(N, M));
+		
+		System.out.println(sb);
+	}
+	
+	
+	// 최대 공약수를 구하는 함수((재귀 방식으로))
+	// gcd: greatest common divisor
+	public static int gcd(int a, int b) {
+		if(b == 0) {
+			return a;
+		}
+		
+		return gcd(b, a % b);
+	}
+	
+	// 반복문 방식
+	public static int gcdByLoop(int a, int b) {
+		while(b != 0) {
+			int r = a % b;
+			
+			a = b;
+			b = r;
+		}
+		
+		return a;
+	}
+}
