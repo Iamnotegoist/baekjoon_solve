e38f166766ec293c4dddfaae792cac60c0cd8c20
kiju9512 <kiju9512@gmail.com>
2024-09-23 13:49:54 +0900

    boj / lkj / 240923 백준 ps 스터디

---
+++ 백트래킹/BOJ_9663.java
@@ -0,0 +1,52 @@
+package 백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * ###N-Queen Problem###
+ */
+public class BOJ_9663 {
+	static int[] arr;
+	static int N;
+	static int count = 0;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		
+		nQueen(0);
+		System.out.println(count);
+	}
+	
+	public static void nQueen(int depth) {
+		if(depth == N) {
+			count++;
+			return;
+		}
+		
+		for(int i = 0; i < N; i++) {
+			arr[depth] = i;
+			
+			if(isPossible(depth)) {
+				nQueen(depth + 1);
+			}
+		}
+	}
+	
+	public static boolean isPossible(int col) {
+		for(int i = 0; i < col; i++) {
+			if(arr[col] == arr[i]) {
+				return false;
+			}
+			
+			else if(Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
